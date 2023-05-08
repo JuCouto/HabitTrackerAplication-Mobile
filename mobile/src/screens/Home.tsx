8cbe6ebf66c4e518f1036d3aebc7f43947908230
Ju Couto <jnalmeida@ymail.com>
2023-05-08 18:00:32 -0300

    Acrescentando importação

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -44,7 +44,7 @@ export function Home(){
                 amountDaysToFill > 0 && Array
                 .from ({length: amountDaysToFill})
                 .map((_, index) => (
-                    <View
+                    <View key={index}
                         className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-700 opacity-40"
                         style={{width: DAY_SIZE, height: DAY_SIZE}}>
                     </View>
