25aeac187e53a003c065f23ec245b18078dd7ed7
Juliana Couto <jnalmeida@ymail.com>
2023-05-12 15:28:45 -0300

    Adicionando checkbox

--- webReact/src/components/HabitDay.tsx
+++ webReact/src/components/HabitDay.tsx
@@ -1,23 +1,57 @@
 
 import * as Popover from '@radix-ui/react-popover';
 import { ProgressBar } from './ProgressBar';
+import clsx from 'clsx';
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { Check } from 'phosphor-react';
 
-// interface HabitDayProps{
-//     completed: number
-// }
+interface HabitDayProps{
+    completed: number
+    amount: number
+}
 
-export function HabitDay(){
+export function HabitDay({completed, amount}: HabitDayProps){
+    const completedPercentage = Math.round((completed / amount) *100)
+console.log(completedPercentage)
     return(
         <Popover.Root>
-        <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg "/> 
+        <Popover.Trigger
+        // Condicionais
+         className={clsx('w-10 h-10 border-2  rounded-lg',{
+            'bg-zinc-900 border-zinc-800' : completedPercentage === 0,
+            'bg-pink-800 border-pink-700' : completedPercentage > 0 && completedPercentage <= 20,
+            'bg-pink-600 border-pink-500' : completedPercentage > 20 && completedPercentage <= 40,
+            'bg-pink-500 border-pink-400' : completedPercentage > 40 && completedPercentage <= 60,
+            'bg-pink-400 border-pink-300': completedPercentage > 60 && completedPercentage <= 80,
+            'bg-pink-300 border-pink-100' : completedPercentage >80,
+
+         } )}/> 
         
         <Popover.Portal>
             <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-800 flex flex-col'>
                 <span className='font-semibold text-zin-400' >terça-feira</span>
                 <span className='mt-1 font-extrabold leading-tight text-3xl'> 09/05</span>
 
-                <ProgressBar progress={45}/>
+                <ProgressBar progress={completedPercentage}/>
 
+                <div className="mt-6 flex flex-col gap-3">
+                    <Checkbox.Root
+                    className="flex items-center gap-3 group">
+
+                        {/* Não consigo estilizar o ckeckbox Indicator, então peciso colocar uma div em volta, e estilizar a div. 
+                        A div não possui data então acrescento o group no checkbox.root para poder acessar os dados do grupo. O group é uma propriedade do tailwind*/}
+                        <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-800 border-2 border-zinc-700 group-data-[state=checked]:bg-green-400 group-data-[state=checked]:border-green-400">
+                        <Checkbox.Indicator> 
+                            <Check size={20} className="text-white"/>
+                        </Checkbox.Indicator> 
+                        </div>
+                      
+
+                       <span className="font-semibold text-xl text-white leaning-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                            Beber 2L de água
+                       </span>
+                    </Checkbox.Root>
+                </div>
              <Popover.Arrow height={8} width={16} className='fill-zinc-800'/>   
             </Popover.Content>
         </Popover.Portal>
--- webReact/src/components/NewHabitForm.tsx
+++ webReact/src/components/NewHabitForm.tsx
@@ -1,24 +1,59 @@
-import { Check } from "phosphor-react";
+import { Check } from 'phosphor-react';
+import * as Checkbox from '@radix-ui/react-checkbox';
 
-export function NewHabitForm(){
-    return(
+export function NewHabitForm() {
+
+    const availableWeekDays = [
+        'Domingo',
+        'Segunda-feira',
+        'Terça-feira',
+        'Quarta-feira',
+        'Quinta-feira',
+        'Sexta-feira',
+        'Sábado'
+    ]
+
+    return (
         <form className="w-full flex flex-col mt-6 ">
             <label htmlFor="title" className="font-semibold leading-tight">
                 Qual o seu comprometimento?
             </label>
 
             <input type="text"
-                   id="title"
-                   placeholder="ex: Beber água, dormir bem, etc..."
-                   className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-pink-200"
-                   autoFocus />
+                id="title"
+                placeholder="ex: Beber água, dormir bem, etc..."
+                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-pink-200"
+                autoFocus />
 
             <label htmlFor="" className="font-semibold leading-tight mt-4">
                 Quais dias na semana?
             </label>
 
+            <div className="flex flex-col gap-2 mt-3">
+                {availableWeekDays.map(weekDay => {
+                    return (
+                        <Checkbox.Root
+                        key={weekDay}
+                            className="flex items-center gap-3 group">
+
+                            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-800 border-2 border-zinc-700 group-data-[state=checked]:bg-green-400 group-data-[state=checked]:border-green-400">
+                                <Checkbox.Indicator>
+                                    <Check size={20} className="text-white" />
+                                </Checkbox.Indicator>
+                            </div>
+
+                            <span className=" text-white leaning-tight ">
+                                {weekDay}
+                            </span>
+                        </Checkbox.Root>
+                    )
+                }
+                )}
+
+            </div>
+
             <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-400 hover:bg-green-300">
-                <Check size={20} weight="bold"/>
+                <Check size={24} weight="bold" />
                 Confirmar
             </button>
         </form>
